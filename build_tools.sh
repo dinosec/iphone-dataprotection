f36fc6425d2ee6be58121c3f7c609d9ea57d2938
dinosec <dinosec@@users.noreply.github.com>
2015-01-24 21:29:33 +0100

    Adding build_tools.sh modifications for OS X Yosemite (v10.10.x)

--- build_tools.sh
+++ build_tools.sh
@@ -6,7 +6,7 @@ then
     exit
 fi
 
-for VER in 5.0 5.1 6.0 6.1 7.0 7.1
+for VER in 5.0 5.1 6.0 6.1 7.0 7.1 8.0 8.1
 do
     if [ -d "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS$VER.sdk/" ];
     then
@@ -31,13 +31,13 @@ if [ ! -f "$SDKPATH/System/Library/Frameworks/IOKit.framework/Headers/IOKitLib.h
     echo "IOKit headers missing"
 
     IOKIT_HDR="/System/Library/Frameworks/IOKit.framework/Headers"
-    IOKIT_HDR_109="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/System/Library/Frameworks/IOKit.framework/Headers"
+    IOKIT_HDR_109="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk/System/Library/Frameworks/IOKit.framework/Headers"
 
     if [ -d $IOKIT_HDR_109 ]; then
         echo "Symlinking headers"
         set -x
         sudo ln -s $IOKIT_HDR_109 "$SDKPATH/System/Library/Frameworks/IOKit.framework/Headers"
-        sudo ln -s "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/usr/include/libkern/OSTypes.h" "$SDKPATH/usr/include/libkern/OSTypes.h"
+        sudo ln -s "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk/usr/include/libkern/OSTypes.h" "$SDKPATH/usr/include/libkern/OSTypes.h"
         set +x
     elif [ -d $IOKIT_HDR ]; then
         echo "Symlinking headers"
